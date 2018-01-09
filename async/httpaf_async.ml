a7e1d5c675d5cf3018e88ebcafa9d78031006dc1
Spiros Eliopoulos <seliopou@gmail.com>
2018-01-09 17:00:52 +0000

    simplify-api: async

--- async/httpaf_async.ml
+++ async/httpaf_async.ml
@@ -1,6 +1,56 @@
 open Core
 open Async
 
+(** XXX(seliopou): Replace Angstrom.Buffered with a module like this, while
+    also supporting growing the buffer. Clients can use this to buffer and the
+    use the unbuffered interface for actually running the parser. *)
+module Buffer : sig
+  type t
+
+  val create   : int -> t
+
+  val get : t -> f:(Bigstring.t -> off:int -> len:int -> int) -> int
+  val put : t -> f:(Bigstring.t -> off:int -> len:int -> int) -> int
+end= struct
+  type t =
+    { buffer      : Bigstring.t
+    ; mutable off : int
+    ; mutable len : int }
+
+  let create size =
+    let buffer = Bigstring.create size in
+    { buffer; off = 0; len = 0 }
+  ;;
+
+  let compress t =
+    if t.len = 0
+    then begin
+      t.off <- 0;
+      t.len <- 0;
+    end else if t.off > 0
+    then begin
+      Bigstring.blit ~src:t.buffer ~src_pos:t.off ~dst:t.buffer ~dst_pos:0 ~len:t.len;
+      t.off <- 0;
+    end
+  ;;
+
+  let get t ~f =
+    let n = f t.buffer ~off:t.off ~len:t.len in
+    t.off <- t.off + n;
+    t.len <- t.len - n;
+    if t.len = 0
+    then t.off <- 0;
+    n
+  ;;
+
+  let put t ~f =
+    compress t;
+    let n = f t.buffer ~off:(t.off + t.len) ~len:(Bigstring.length t.buffer - t.len) in
+    t.len <- t.len + n;
+    n
+  ;;
+end
+
 let read fd buffer =
   let badfd fd = failwithf "read got back fd: %s" (Fd.to_string fd) () in
   let rec finish fd buffer result =
@@ -25,15 +75,17 @@ let read fd buffer =
       finish fd buffer
         (Fd.syscall fd ~nonblocking:true
           (fun file_descr ->
-            Unix.Syscall_result.Int.ok_or_unix_error_exn ~syscall_name:"read"
-              (Bigstring.read_assume_fd_is_nonblocking file_descr buffer)))
+            Buffer.put buffer ~f:(fun bigstring ~off ~len ->
+              Unix.Syscall_result.Int.ok_or_unix_error_exn ~syscall_name:"read"
+                (Bigstring.read_assume_fd_is_nonblocking file_descr bigstring ~pos:off ~len))))
     else
       Fd.syscall_in_thread fd ~name:"read"
-        (fun file_descr -> Bigstring.read file_descr buffer)
+        (fun file_descr ->
+          Buffer.put buffer ~f:(fun bigstring ~off ~len ->
+            Bigstring.read file_descr bigstring ~pos:off ~len))
       >>= fun result -> finish fd buffer result
   in
   go fd buffer
-
 
 open Httpaf
 
@@ -46,13 +98,23 @@ module Server = struct
       let error_handler   = error_handler client_addr in
       let conn = Server_connection.create ?config ~error_handler request_handler in
       let read_complete = Ivar.create () in
+      (* XXX(seliopou): Make this configurable *)
+      let buffer = Buffer.create 0x1000 in
       let rec reader_thread () =
         match Server_connection.next_read_operation conn with
-        | `Read buffer ->
+        | `Read ->
           (* Log.Global.printf "read(%d)%!" (Fd.to_int_exn fd); *)
-          read fd buffer >>> fun result ->
-            Server_connection.report_read_result conn result;
-            reader_thread ()
+          read fd buffer
+          >>> begin function
+            | `Eof  ->
+              Server_connection.shutdown_reader conn;
+              reader_thread ()
+            | `Ok _ ->
+              Buffer.get buffer ~f:(fun bigstring ~off ~len ->
+                Server_connection.read conn bigstring ~off ~len)
+              |> ignore;
+              reader_thread ()
+          end
         | `Yield  ->
           (* Log.Global.printf "read_yield(%d)%!" (Fd.to_int_exn fd); *)
           Server_connection.yield_reader conn reader_thread
@@ -100,13 +162,22 @@ module Client = struct
     let request_body, conn   =
       Client_connection.request request ~error_handler ~response_handler in
     let read_complete = Ivar.create () in
+    let buffer = Buffer.create 0x1000 in
     let rec reader_thread () =
       match Client_connection.next_read_operation conn with
-      | `Read buffer ->
+      | `Read ->
         (* Log.Global.printf "read(%d)%!" (Fd.to_int_exn fd); *)
-        read fd buffer >>> fun result ->
-          Client_connection.report_read_result conn result;
-          reader_thread ()
+        read fd buffer
+          >>> begin function
+            | `Eof  ->
+              Client_connection.shutdown_reader conn;
+              reader_thread ()
+            | `Ok _ ->
+              Buffer.get buffer ~f:(fun bigstring ~off ~len ->
+                Client_connection.read conn bigstring ~off ~len)
+              |> ignore;
+              reader_thread ()
+          end
       | `Close ->
         (* Log.Global.printf "read_close(%d)%!" (Fd.to_int_exn fd); *)
         Ivar.fill read_complete ();
