676d64dcc0df276250aa58b9691967b03afe17fd
Spiros Eliopoulos <seliopou@gmail.com>
2018-02-19 23:50:08 +0000

    fix-iovec-shiftv: add tests that demonstarate the bug
    Pull request #33 purported to fix a bug, and did identify the presence
    of a bug, but did not completely address it. It was not sufficient to
    swap the comparison, but also modify the false branch to only shift off
    `n` from the iovec at the head of the list, rather than the length of
    the iovec itself.
    
    The test also identifies a misleading exception message, namely
    
      "shiftv: n >= lengthv iovecs"
    
    That should be a strict inequality.
    
    Both these issues will be fixed in subsequent commits.

--- lib_test/test_httpaf.ml
+++ lib_test/test_httpaf.ml
@@ -67,8 +67,49 @@ module Method = struct
     ]
 end
 
+module IOVec = struct
+  include IOVec
+
+  (* The length of the buffer is ignored by iovec operations *)
+  let buffer = Bigstring.create 0
+
+  let test_lengthv () =
+    Alcotest.(check int) "lengthv [] = 0"                 (lengthv []) 0;
+    Alcotest.(check int) "lengthv [iovec] = length iovec"
+      (lengthv [{ buffer; off = 0; len = 0 }]) (length {buffer; off = 0; len = 0 });
+    Alcotest.(check int) "lengthv [iovec] = length iovec"
+      (lengthv [{ buffer; off = 0; len = 10 }]) (length {buffer; off = 0; len = 10 });
+  ;;
+
+  let test_shiftv_raises () =
+    let test f =
+      Alcotest.check_raises
+        "shiftv iovecs n raises when n > lengthv iovecs"
+        (Failure "shiftv: n > lengthv iovecs")
+      (fun () -> ignore (f ()))
+    in
+    test (fun () -> shiftv [] 1);
+    test (fun () -> shiftv [{ buffer; off = 0; len = 1 }] 2);
+    test (fun () -> shiftv [{ buffer; off = 0; len = 1 }; { buffer; off = 0; len = 1 }] 3);
+  ;;
+
+  let test_shiftv () =
+    Alcotest.(check (of_pp pp_hum |> list)) "shiftv [] 0 = []" (shiftv [] 0) [];
+    Alcotest.(check (of_pp pp_hum |> list)) "shiftv [{... len ... }] len = []"
+      (shiftv [{ buffer; off = 0; len = 1 }] 1) [];
+    Alcotest.(check (of_pp pp_hum |> list)) "shiftv [iovec] n when length iovec < n"
+      (shiftv [{ buffer; off = 0; len = 4 }] 2) [{ buffer; off = 2; len = 2 }];
+  ;;
+
+  let tests =
+    [ "lengthv"       , `Quick, test_lengthv
+    ; "shiftv"        , `Quick, test_shiftv
+    ; "shiftv raises ", `Quick, test_shiftv_raises ]
+end
+
 let () =
   Alcotest.run "httpaf unit tests"
     [ "version" , Version.tests
     ; "method"  , Method.tests
+    ; "iovec"   , IOVec.tests
     ]
