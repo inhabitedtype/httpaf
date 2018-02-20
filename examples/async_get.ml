fc10ec0b95b91ae2044febe4ad53c4bdbfc030bb
Spiros Eliopoulos <seliopou@gmail.com>
2018-02-20 11:46:03 +0000

    update-examples: async_get

--- examples/async_get.ml
+++ examples/async_get.ml
@@ -12,34 +12,38 @@ let response_handler finished response response_body =
       Body.schedule_read response_body ~on_read ~on_eof
     and on_eof () = Ivar.fill finished () in
     Body.schedule_read response_body ~on_read ~on_eof;
+  | response ->
+    Format.fprintf Format.std_formatter "%a\n%!" Response.pp_hum response;
+    Core.exit 1
 ;;
 
 let error_handler _ = assert false
 
-let main port address () =
-  let where_to_connect = Tcp.to_host_and_port address port in
+let main port host () =
+  let where_to_connect = Tcp.Where_to_connect.of_host_and_port { host; port } in
   let finished = Ivar.create () in
   Tcp.connect_sock where_to_connect
   >>= fun socket ->
+    let headers = Headers.of_list [ "host", host ] in
     let request_body =
       Client.request
         ~error_handler
         ~response_handler:(response_handler finished)
         socket
-        (Request.create `GET "/")
+        (Request.create ~headers `GET "/")
     in
     Body.close request_body;
     Ivar.read finished
 ;;
 
 let () =
-  Command.async
+  Command.async_spec
     ~summary:"Start a hello world Async server"
     Command.Spec.(empty +>
       flag "-p" (optional_with_default 80 int)
         ~doc:"int destination port"
       +>
-      flag "-a" (required string)
-        ~doc:"string destination ip"
+      flag "-h" (required string)
+        ~doc:"string destination host"
     ) main
   |> Command.run
