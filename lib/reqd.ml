661a0f44e73986086836dd635282d3e60a61c4bc
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2018-11-19 16:51:24 +0000

    Fix and add test

--- lib/reqd.ml
+++ lib/reqd.ml
@@ -148,7 +148,7 @@ let respond_with_bigstring t response (bstr:Bigstring.t) =
     failwith "httpaf.Reqd.respond_with_bigstring: response already complete"
 
 let unsafe_respond_with_streaming ~flush_headers_immediately t response =
-  t.wait_for_first_flush <- flush_headers_immediately;
+  t.wait_for_first_flush <- not flush_headers_immediately;
   match t.response_state with
   | Waiting when_done_waiting ->
     let response_body = Body.create t.response_body_buffer in
@@ -164,7 +164,7 @@ let unsafe_respond_with_streaming ~flush_headers_immediately t response =
   | Complete _ ->
     failwith "httpaf.Reqd.respond_with_streaming: response already complete"
 
-let respond_with_streaming ?(flush_headers_immediately=true) t response =
+let respond_with_streaming ?(flush_headers_immediately=false) t response =
   if t.error_code <> `Ok then
     failwith "httpaf.Reqd.respond_with_streaming: invalid state, currently handling error";
   unsafe_respond_with_streaming ~flush_headers_immediately t response
@@ -181,7 +181,7 @@ let report_error t error =
       | #Status.standard as status -> status
     in
     t.error_handler ~request:t.request error (fun headers ->
-      unsafe_respond_with_streaming ~flush_headers_immediately:false t (Response.create ~headers status))
+      unsafe_respond_with_streaming ~flush_headers_immediately:true t (Response.create ~headers status))
   | Waiting _, `Exn _ ->
     (* XXX(seliopou): Decide what to do in this unlikely case. There is an
      * outstanding call to the [error_handler], but an intervening exception
