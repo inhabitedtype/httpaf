c3c5da46f6dddbbdcee1d17bcc76896a023d6d0d
Spiros Eliopoulos <seliopou@gmail.com>
2020-05-17 11:47:52 -0400

    branch-free-equal: add case-insensitive char equality check
    Exhaustively check all character pairs and ensure that the naive
    implementation of case-insensitive equality checking is consistent with
    the one used in the Headers module. Getting a value for the latter is
    accomplished by constructing a Headers.t that contains just one entry,
    with its name being one of the characters to check, and checking for
    membership using the other character.

--- lib_test/test_headers.ml
+++ lib_test/test_headers.ml
@@ -40,7 +40,31 @@ let test_remove () =
       "a");
 ;;
 
+let test_ci_equal () =
+  let ascii = List.init (0x7f + 1) Char.chr in
+  let string_of_char x = String.init 1 (fun _ -> x) in
+  (* Ensure that the branch free case-insensitive equality check is consistent
+   * with a naive implementation. *)
+  List.iter2 (fun x y ->
+    let char_ci_equal =
+      Char.compare (Char.lowercase_ascii x) (Char.lowercase_ascii y) = 0
+    in
+    let headers_equal =
+      let headers = Headers.of_list [ string_of_char y, "value" ] in
+      Headers.mem headers (string_of_char x)
+    in
+    Alcotest.(check bool)
+      (Printf.sprintf "CI: %c = %c" x y)
+      char_ci_equal
+      headers_equal)
+  ascii
+  ascii
+;;
+
+
+
 let tests =
-  [ "remove" , `Quick, test_remove
-  ; "replace", `Quick, test_replace
+  [ "remove"  , `Quick, test_remove
+  ; "replace" , `Quick, test_replace
+  ; "CI equal", `Quick, test_ci_equal
   ]
